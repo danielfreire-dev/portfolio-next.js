3410efda87d3b0db437b72a3bb028d62cd87aadf
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2026-02-22 17:13:33 +0000

    new ::selection txt color

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -146,7 +146,7 @@ export default async function RootLayout({
 			</Suspense>
 			<body
 				className={
-					`${Logo.variable} ${Heading.variable} ${Text.variable} ${Small.variable} ${SmallItalic.variable}  antialiased flex min-h-screen selection:bg-(--primary) selection:text-(--hover-text) ` /* stars */
+					`${Logo.variable} ${Heading.variable} ${Text.variable} ${Small.variable} ${SmallItalic.variable}  antialiased flex min-h-screen selection:bg-(--primary) selection:text-(--selector-txt) ` /* stars */
 				}
 			>
 				<NextIntlClientProvider messages={messages}>
